b2086c41efb1554f4da8a4ffe841c4036d6d33a5
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-12-05 14:08:28 +0900

    UITableViewのサンプル

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let names = [
+        "John Smith",
+        "Dan Smith",
+        "Jason Smith",
+        "Mary Smith"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        print("you tapped me!")
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        
+        return cell
+    }
+}
